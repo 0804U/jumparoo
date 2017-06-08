8e9d554e9a30d0abfc174ca05aed807c601e879c
gary <ggrumbley@ucsd.edu>
2017-06-08 11:05:37 -0700

    Add score HUD

--- js/main.js
+++ js/main.js
@@ -106,6 +106,8 @@ PlayState.init = function () {
         this.sfx.jump.play();
     }
 }, this);
+
+this.coinPickupCount = 0;
 };
 
 // Load Game Assets
@@ -120,6 +122,8 @@ PlayState.preload = function() {
   this.game.load.image('grass:1x1', 'images/grass_1x1.png');
   this.game.load.image('hero', 'images/hero_stopped.png');
   this.game.load.image('invisible-wall', 'images/invisible_wall.png');
+  this.game.load.image('icon:coin', 'images/coin_icon.png');
+  this.game.load.image('font:numbers', 'images/numbers.png');
   this.game.load.spritesheet('coin', 'images/coin_animated.png', 22, 22);
   this.game.load.spritesheet('spider', 'images/spider.png', 42, 32);
   this.game.load.audio('sfx:jump', 'audio/jump.wav');
@@ -185,6 +189,7 @@ PlayState._spawnCoin = function (coin) {
 PlayState._onHeroVsCoin = function (hero, coin) {
   this.sfx.coin.play();
   coin.kill();
+  this.coinPickupCount++;
 };
 
 // Hero and Spider Collision Helper
@@ -250,6 +255,23 @@ PlayState._loadLevel = function (data) {
   this.game.physics.arcade.gravity.y = GRAVITY;
 };
 
+// HUD Helper
+PlayState._createHud = function () {
+  const NUMBERS_STR = '0123456789X ';
+  this.coinFont = this.game.add.retroFont('font:numbers', 20, 26,
+      NUMBERS_STR);
+
+  let coinIcon = this.game.make.image(0, 0, 'icon:coin');
+  let coinScoreImg = this.game.make.image(coinIcon.x + coinIcon.width,
+      coinIcon.height / 2, this.coinFont);
+  coinScoreImg.anchor.set(0, 0.5);
+
+  this.hud = this.game.add.group();
+  this.hud.add(coinIcon);
+  this.hud.add(coinScoreImg);
+  this.hud.position.set(10, 10);
+};
+
 // Create Game Entities
 PlayState.create = function() {
   // Sound Entities
@@ -261,12 +283,14 @@ PlayState.create = function() {
 
   this.game.add.image(0, 0, 'background');
   this._loadLevel(this.game.cache.getJSON('level:1'));
+  this._createHud();
 }
 
 // Update Game State
 PlayState.update = function () {
   this._handleCollisions();
   this._handleInput();
+  this.coinFont.text = `x${this.coinPickupCount}`;
 }
 
 // =============================================================================
